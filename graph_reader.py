0053766a7b071414eba44ca8ce55cb1300ea266c
Eric Brandwein <brandweineric@gmail.com>
2023-04-18 12:36:30 -0300

    Read nodetypes int

--- graph_reader.py
+++ graph_reader.py
@@ -8,4 +8,4 @@ GRAPHS_FILE_EXTENSION = ".adjlist"
 def read_graph(filename):
     path = f"{GRAPHS_DIR}/{filename}{GRAPHS_FILE_EXTENSION}"
     print("Reading graph from file:", path)
-    return nx.read_adjlist(path)
+    return nx.read_adjlist(path, nodetype=int)
