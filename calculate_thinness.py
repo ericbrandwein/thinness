a3c02722930c76ed43dabf0e4d4addc67d3710fe
Eric Brandwein <brandweineric@gmail.com>
2023-04-18 20:24:08 -0300

    Return consistent solution

--- calculate_thinness.py
+++ calculate_thinness.py
@@ -6,14 +6,31 @@ GRAPHS_DIR = "adjlists"
 GRAPHS_FILE_EXTENSION = ".adjlist"
 
 
-def find_first_adjacent(G, nodes, node):
+class ConsistentSolution:
+    def __init__(self, ordering, partition):
+        self.ordering = ordering
+        self.partition = partition
+
+    @property
+    def number_of_classes(self):
+        return len(set(self.partition.values()))
+    
+    @property
+    def complete_partition(self):
+        return {
+            node: self.partition[node] if node in self.partition else 0 
+            for node in self.ordering
+        }
+
+
+def _find_first_adjacent(G, nodes, node):
     return next(
         (index for index, adjacent in enumerate(nodes) if G.has_edge(adjacent, node)), 
         None
     )
 
 
-def add_last_constraints(G, constraints_graph, before, new_node):
+def _add_last_constraints(G, constraints_graph, before, new_node):
     for u_index, u in enumerate(before):
         if G.has_edge(u, new_node):
             for v in before[u_index + 1:]:
@@ -21,48 +38,47 @@ def add_last_constraints(G, constraints_graph, before, new_node):
                     constraints_graph.add_edge(u, v)
 
 
-def add_middle_constraints(G, constraints_graph, before, after, new_node):
+def _add_middle_constraints(G, constraints_graph, before, after, new_node):
     nonadjacent_after = [
         nonadjacent for nonadjacent in after if not G.has_edge(new_node, nonadjacent)
     ]
     for u in before:
-        if find_first_adjacent(G, nonadjacent_after, u) is not None:
+        if _find_first_adjacent(G, nonadjacent_after, u) is not None:
             constraints_graph.add_edge(u, new_node)
          
 
-def add_first_constraints(G, constraints_graph, after, new_node):
+def _add_first_constraints(G, constraints_graph, after, new_node):
     for v_index, v in enumerate(after):
         nonadjacent_v = [
             nonadjacent for nonadjacent in after[v_index + 1:] if not G.has_edge(v, nonadjacent)
         ]
-        if find_first_adjacent(G, nonadjacent_v, new_node) is not None:
+        if _find_first_adjacent(G, nonadjacent_v, new_node) is not None:
             constraints_graph.add_edge(new_node, v)
 
 
-def add_constraints(G, constraints_graph, before, after, new_node):
-    add_last_constraints(G, constraints_graph, before, new_node)
-    add_middle_constraints(G, constraints_graph, before, after, new_node)
-    add_first_constraints(G, constraints_graph, after, new_node)
+def _add_constraints(G, constraints_graph, before, after, new_node):
+    _add_last_constraints(G, constraints_graph, before, new_node)
+    _add_middle_constraints(G, constraints_graph, before, after, new_node)
+    _add_first_constraints(G, constraints_graph, after, new_node)
 
 
-def extend_constraints_graph(G, constraints_graph, ordering, new_node):
+def _extend_constraints_graph(G, constraints_graph, ordering, new_node):
     new_graph = constraints_graph.copy()
     position = ordering.index(new_node)
     before = ordering[:position]
     after = ordering[position + 1:]
-    add_constraints(G, new_graph, before, after, new_node)
+    _add_constraints(G, new_graph, before, after, new_node)
     return new_graph
 
 
-def cocomparability_coloring(graph):
-    coloring = nx.coloring.greedy_color(graph)
-    return len(set(coloring.values()))
+def _cocomparability_coloring(graph):
+    return nx.coloring.greedy_color(graph)
 
 
-def calculate_thinness_for_ordering(G, constraints_graph, ordering, new_node):
-    constraints_graph = extend_constraints_graph(G, constraints_graph, ordering, new_node)
-    number = cocomparability_coloring(constraints_graph)
-    return number, constraints_graph
+def _calculate_thinness_for_ordering(G, constraints_graph, ordering, new_node):
+    constraints_graph = _extend_constraints_graph(G, constraints_graph, ordering, new_node)
+    coloring = _cocomparability_coloring(constraints_graph)
+    return coloring, constraints_graph
 
 
 def insertions(iterable, element):
@@ -70,51 +86,71 @@ def insertions(iterable, element):
         yield iterable[:i] + [element] + iterable[i:]
 
 
-def calculate_thinness_starting_with_suborder(G, constraints_graph, ordering, new_node, remaining_nodes, lower_bound: int, upper_bound: int):
-    subG = nx.induced_subgraph(G, ordering)
-    thinness, constraints_graph = calculate_thinness_for_ordering(
-        subG, constraints_graph, ordering, new_node
-    )
-    if thinness >= upper_bound:
-        return upper_bound
-    if len(remaining_nodes) == 0:
-        return thinness
+def _calculate_thinness_for_strict_suborder(G, constraints_graph, ordering, remaining_nodes, lower_bound: int, upper_bound: int) -> ConsistentSolution | None:
+    best_solution = None
     next_node = remaining_nodes.pop()
     for new_ordering in insertions(ordering, next_node):
-        thinness = calculate_thinness_starting_with_suborder(
+        solution = _calculate_thinness_for_suborder(
             G, constraints_graph, new_ordering, next_node, remaining_nodes, lower_bound, upper_bound
         )
-        upper_bound = min(upper_bound, thinness)
-        if upper_bound <= lower_bound:
-            break
+        if solution:
+            new_thinness = solution.number_of_classes
+            if new_thinness < upper_bound:
+                upper_bound = new_thinness
+                best_solution = solution
+            if upper_bound <= lower_bound:
+                break
     remaining_nodes.add(next_node)
-    return upper_bound
+    return best_solution
 
 
-def calculate_thinness_backtracking(G, lower_bound=None, upper_bound=None):
+def _calculate_thinness_for_suborder(G, constraints_graph, ordering, new_node, remaining_nodes, lower_bound: int, upper_bound: int) -> ConsistentSolution | None:
+    subG = nx.induced_subgraph(G, ordering)
+    best_partition, constraints_graph = _calculate_thinness_for_ordering(
+        subG, constraints_graph, ordering, new_node
+    )
+    best_solution = ConsistentSolution(ordering, best_partition)
+    thinness = best_solution.number_of_classes
+    if thinness >= upper_bound:
+        return None
+    if len(remaining_nodes) == 0:
+        return best_solution
+    
+    return _calculate_thinness_for_strict_suborder(G, constraints_graph, ordering, remaining_nodes, lower_bound, upper_bound)
+
+
+def calculate_thinness_backtracking(G, lower_bound=1, upper_bound=None):
     nodes = set(G.nodes())
     ordering = [nodes.pop()]
-    return calculate_thinness_starting_with_suborder(
-        G, nx.Graph(), ordering, ordering[0], nodes, 
-        lower_bound or 1,
-        upper_bound or G.number_of_nodes()
+    return _calculate_thinness_for_suborder(
+        G, nx.Graph(), ordering, ordering[0], nodes, lower_bound,
+        upper_bound or max(1, G.number_of_nodes() - 1)
     )
 
 
-def parse_arguments():
+def calculate_thinness(G, lower_bound=None, upper_bound=None):
+    return calculate_thinness_backtracking(G, lower_bound, upper_bound)
+
+
+def _parse_arguments():
     if len(sys.argv) < 2:
         return "graph"
     else:
         return sys.argv[1]
 
 
-def main():
-    graph_file = parse_arguments()
+def _main():
+    graph_file = _parse_arguments()
     G = read_graph(graph_file)
-    
-    print("Thinness:", calculate_thinness_backtracking(G))
+    solution = calculate_thinness_backtracking(G)
+    if solution:
+        print("Thinness:", solution.number_of_classes)
+        print("Partition:", solution.complete_partition)
+        print("Ordering:", solution.ordering)
+    else:
+        print("No solution found")
 
 
 if __name__ == "__main__":
-    main()
+    _main()
     
