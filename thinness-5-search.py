c9c5371f05726514b4d363550d26b20167ae69b3
Eric Brandwein <brandweineric@gmail.com>
2023-04-19 10:37:37 -0300

    Create graph_extensions

--- thinness-5-search.py
+++ thinness-5-search.py
@@ -5,24 +5,28 @@ import networkx as nx
 from utils import all_subsets
 
 
+def graph_extensions(G):
+    for subset in all_subsets(G.nodes):
+        new_graph = G.copy()
+        new_graph.add_edges_from((new_node, old_node) for old_node in subset)
+        yield new_graph
+
+
 # Is there a 9-vertex graph with thinness 5?
 co4K2 = read_graph("co-4K2")
 new_node = 9
 found = False
-for subset in all_subsets(co4K2.nodes):
-    print(subset)
-    new_graph = co4K2.copy()
-    new_graph.add_edges_from((new_node, old_node) for old_node in subset)
-    new_solution = calculate_thinness(new_graph, lower_bound=4, upper_bound=5)
-    if not verify_solution(new_graph, new_solution):
+for graph in graph_extensions(co4K2):
+    new_solution = calculate_thinness(graph, lower_bound=4, upper_bound=5)
+    if not verify_solution(graph, new_solution):
         print("Solution is not correct!")
         print(new_solution)
-        show_graph(new_graph)
+        show_graph(graph)
     if new_solution.number_of_classes == 5:
         print("Found a graph with thinness 5!")
-        nx.write_adjlist(new_graph, "adjlits/9-nodes-5-thinness.adjlist")
+        nx.write_adjlist(graph, "adjlits/9-nodes-5-thinness.adjlist")
         found = True
-        show_graph(new_graph)
+        show_graph(graph)
         break
 
 if not found:
