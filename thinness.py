e3ab8fdc256683d594daf86bab209246902a4907
Eric Brandwein <brandweineric@gmail.com>
2023-08-19 18:57:54 -0300

    Calculate thinness in multiple processes

--- thinness.py
+++ thinness.py
@@ -1,7 +1,16 @@
 import csv
 import os
+import sys
+import time
+import multiprocessing as mp
+
 from z3_thinness import calculate_thinness_with_z3
 from helpers import *
+
+
+GRAPHS_WITH_N_10 = 11716571
+CHUNK_SIZE = 50
+LAST_PROCESSED_FILENAME = 'data/last-processed.graph6'
 
 
 def graphs_by_thinness(n, *, minimal_only=True):
@@ -57,50 +66,75 @@ def graphs_by_thinness_precomputed(n=8):
     return output_dict
 
 
-def write_graph_to_csv(G, filename):
+def write_graph_to_csv(graph6, filename):
     with open(filename, mode='a', newline='') as csvfile:
         writer = csv.writer(csvfile)
-        writer.writerow([G.graph6_string(), G.name(), ''])
+        writer.writerow([graph6, '', ''])
 
 
-def save_graph_with_thinness(G, thinness):
-    write_graph_to_csv(G, f'data/thinness-{thinness}.csv')
+def save_graph_with_thinness(graph6, thinness):
+    write_graph_to_csv(graph6, f'data/thinness-{thinness}.csv')
 
 
-def save_last_processed_graph(G):
-    with open('data/last-processed.graph6', mode='w', newline='') as file:
-        file.write(G.graph6_string())
+def save_last_processed_graph(graph6):
+    with open(LAST_PROCESSED_FILENAME, mode='w', newline='') as file:
+        file.write(graph6)
 
 
 def get_last_processed_graph6():
-    if os.path.exists('data/last-processed.graph6'):
-        with open('data/last-processed.graph6', mode='r', newline='') as file:
+    if os.path.exists(LAST_PROCESSED_FILENAME):
+        with open(LAST_PROCESSED_FILENAME, mode='r', newline='') as file:
             return file.read()
     return None
 
 
-def process_graph(G, graphs_dict):
+def process_graph(params):
+    G, graphs_dict = params
     lower_bound = find_lower_bound(G, graphs_dict)
     k, _, _ = calculate_thinness_with_z3(G, lower_bound=lower_bound) 
-    if k > 1:
-        graphs_dict.setdefault(k, [])
-        if not has_induced_subgraph(G, graphs_dict[k]):
-            save_graph_with_thinness(G, k)
-            print('!!FOUND!!', G.graph6_string(), 'Thinness:', k, 'Vertices:', len(G.vertices()))
-    save_last_processed_graph(G)
+    is_minimal = k > 1 and not has_induced_subgraph(G, graphs_dict[k])
+    return G.graph6_string(), k, is_minimal
 
 
-def fill_csvs():
+def print_updated_progress(index):
+    if index % CHUNK_SIZE == 0:
+        print(f'{index:,}/{GRAPHS_WITH_N_10:,} graphs processed, {GRAPHS_WITH_N_10 - index:,} remaining...', end='\r')
+
+
+def print_found_graph(graph6, thinness):
+    sys.stdout.write(u"\u001b[2K")
+    print('Found minimal graph:', graph6, 'Thinness:', thinness)
+
+
+def skip_graphs_until(graphs, graph6):
+    for index, graph in enumerate(graphs, start = 1):
+        if graph.graph6_string() == graph6:
+            return index
+
+
+def skip_processed_graphs(graphs):
     last_processed = get_last_processed_graph6()
-    graphs_dict = graphs_by_thinness_precomputed()
-    last_processed_found = last_processed is None
-    for index, G in enumerate(connected_graphs_upto(9, start=9), start=1):
-        if index % 1000 == 0:
-            print('Graph number', index, 'reached')
-        if not last_processed_found:
-            graph6 = G.graph6_string()
-            last_processed_found = graph6 == last_processed
-            print('Not processing', graph6)
-            continue
-        process_graph(G, graphs_dict)
+    if last_processed:
+        print('Skipping graphs until', last_processed)
+        return skip_graphs_until(graphs, last_processed)
+    else:
+        return 0
 
+
+def fill_csvs_paralelly(n=10):
+    graphs_dict = graphs_by_thinness_precomputed(n=n-1)
+    graphs_dict.setdefault(int(n/2), [])
+    graphs = connected_graphs_upto(n, start=n)
+    graphs_skipped = skip_processed_graphs(graphs)
+        
+    with mp.Pool() as pool:
+        params = ((G, graphs_dict) for G in graphs)
+        process_map = pool.imap(process_graph, params, chunksize=CHUNK_SIZE)
+        for index, (graph6, thinness, is_minimal) in enumerate(process_map, start=graphs_skipped + 1):
+            if is_minimal:
+                save_graph_with_thinness(graph6, thinness)
+                print_found_graph(graph6, thinness)
+            save_last_processed_graph(graph6)
+            print_updated_progress(index)
+                
+                
