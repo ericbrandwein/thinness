5a3f47467a62594f3f1e72598eeabaf98a1661fe
Eric Brandwein <brandweineric@gmail.com>
2023-08-22 16:51:12 -0300

    Print time remaining

--- thinness.py
+++ thinness.py
@@ -1,7 +1,7 @@
 import csv
 import os
 import sys
-import time
+from datetime import datetime
 import multiprocessing as mp
 
 from z3_thinness import calculate_thinness_with_z3
@@ -12,38 +12,6 @@ GRAPHS_WITH_N_10 = 11716571
 CHUNK_SIZE = 50
 LAST_PROCESSED_INDEX_FILENAME = 'data/last-processed.index'
 
-
-def graphs_by_thinness(n, *, minimal_only=True):
-    r""" Calculates the thinness of connected non-isomorphic graphs up to `n` vertices.
-
-    TESTS::
-
-        sage: {k: len(l) for k, l in graphs_by_thinness(4, minimal_only=False).items()}
-        {1: 8, 2: 1}
-        sage: {k: len(l) for k, l in graphs_by_thinness(5, minimal_only=False).items()}
-        {1: 23, 2: 7}
-        sage: {k: len(l) for k, l in graphs_by_thinness(6, minimal_only=False).items()}
-        {1: 79, 2: 62, 3: 1}
-        sage: {k: len(l) for k, l in graphs_by_thinness(4).items()}
-        {1: 1, 2: 1}
-        sage: {k: len(l) for k, l in graphs_by_thinness(5).items()}
-        {1: 1, 2: 2}
-        sage: {k: len(l) for k, l in graphs_by_thinness(6).items()}
-        {1: 1, 2: 5, 3: 1}
-        sage: [G for G in _connected_graphs_upto(6) if G.is_interval()] == graphs_by_thinness(6, minimal_only=False)[1]
-        True
-    """
-    graphs_dict = {}
-    for G in connected_graphs_upto(n):
-        lower_bound = find_lower_bound(G, graphs_dict)
-        k, _, _ = calculate_thinness_with_z3(G, lower_bound=lower_bound) 
-        graphs_dict.setdefault(k, [])
-        if minimal_only:
-            if not has_induced_subgraph(G, graphs_dict[k]):
-                graphs_dict[k].append(G)
-        else:
-            graphs_dict[k].append(G)
-    return graphs_dict
 
 def graphs_by_thinness_precomputed(n=8):
     r""" Outputs the same as `graphs_by_thinness` by using precomputed values.
@@ -96,9 +64,18 @@ def process_graph(params):
     return G.graph6_string(), k, is_minimal
 
 
-def print_updated_progress(index):
+def estimate_time_remaining(start_time, graphs_processed, graphs_remaining):
+    elapsed = datetime.today() - start_time
+    time_per_graph = elapsed / graphs_processed
+    return time_per_graph * graphs_remaining
+
+
+def print_updated_progress(index, graphs_skipped, start_time):
     if index % CHUNK_SIZE == 0:
-        print(f'{index:,}/{GRAPHS_WITH_N_10:,} graphs processed, {GRAPHS_WITH_N_10 - index:,} remaining...', end='\r')
+        graphs_processed = index - graphs_skipped
+        graphs_remaining = GRAPHS_WITH_N_10 - index
+        time_remaining = estimate_time_remaining(start_time, graphs_processed, graphs_remaining)
+        print(f'{index:,} total graphs processed, {graphs_remaining:,} remaining. Time remaining: {time_remaining}', end='\r')
 
 
 def print_found_graph(graph6, thinness):
@@ -108,14 +85,17 @@ def print_found_graph(graph6, thinness):
 
 def skip_graphs_until(graphs, index):
     for curr_index, _ in enumerate(graphs, start = 1):
+        if curr_index % 1000 == 0:
+            print(f'Skipped {curr_index:,}/{index:,} graphs...', end='\r')
         if curr_index == index:
+            print()
             break
 
 
 def skip_processed_graphs(graphs):
     last_processed = get_last_processed_index()
-    if last_processed> 0:
-        print('Skipping first', last_processed, 'graphs...')
+    if last_processed > 0:
+        print(f'Skipping first {last_processed:,} graphs...')
         skip_graphs_until(graphs, last_processed)
     return last_processed
 
@@ -125,7 +105,8 @@ def fill_csvs_paralelly(n=10):
     graphs_dict.setdefault(int(n/2), [])
     graphs = connected_graphs_upto(n, start=n)
     graphs_skipped = skip_processed_graphs(graphs)
-        
+    
+    start_time = datetime.today()
     with mp.Pool() as pool:
         params = ((G, graphs_dict) for G in graphs)
         process_map = pool.imap(process_graph, params, chunksize=CHUNK_SIZE)
@@ -134,4 +115,4 @@ def fill_csvs_paralelly(n=10):
                 save_graph_with_thinness(graph6, thinness)
                 print_found_graph(graph6, thinness)
             save_last_processed_index(index)
-            print_updated_progress(index)
+            print_updated_progress(index, graphs_skipped, start_time)
