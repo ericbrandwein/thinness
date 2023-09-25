f234188c1104bcf42b1d7940372f3b5337470f7d
Eric Brandwein <brandweineric@gmail.com>
2023-09-25 21:13:28 +0000

    Fix index things

--- thinness.py
+++ thinness.py
@@ -13,7 +13,7 @@ CHUNK_SIZE = 50
 LAST_PROCESSED_INDEX_FILENAME = 'data/last-processed.index'
 
 
-def graphs_by_thinness_precomputed(n=8):
+def graphs_by_thinness_precomputed(n):
     r""" Outputs the same as `graphs_by_thinness` by using precomputed values.
 
     TESTS::
@@ -53,7 +53,7 @@ def get_last_processed_index():
     if os.path.exists(LAST_PROCESSED_INDEX_FILENAME):
         with open(LAST_PROCESSED_INDEX_FILENAME, mode='r', newline='') as file:
             return int(file.read())
-    return None
+    return -1
 
 
 def process_graph(params):
@@ -70,12 +70,12 @@ def estimate_time_remaining(start_time, graphs_processed, graphs_remaining):
     return time_per_graph * graphs_remaining
 
 
-def print_updated_progress(index, graphs_skipped, start_time):
+def print_updated_progress(index, last_skipped_graph, start_time):
     if index % CHUNK_SIZE == 0:
-        graphs_processed = index - graphs_skipped
-        graphs_remaining = GRAPHS_WITH_N_10 - index
+        graphs_processed = index - last_skipped_graph
+        graphs_remaining = GRAPHS_WITH_N_10 - index - 1
         time_remaining = estimate_time_remaining(start_time, graphs_processed, graphs_remaining)
-        print(f'{index:,} total graphs processed, {graphs_remaining:,} remaining. Time remaining: {time_remaining}', end='\r')
+        print(f'{index + 1:,} total graphs processed, {graphs_remaining:,} remaining. Time remaining: {time_remaining}', end='\r')
 
 
 def print_found_graph(graph6, thinness):
@@ -84,9 +84,9 @@ def print_found_graph(graph6, thinness):
 
 
 def skip_graphs_until(graphs, index):
-    for curr_index, _ in enumerate(graphs, start = 1):
+    for curr_index, _ in enumerate(graphs):
         if curr_index % 1000 == 0:
-            print(f'Skipped {curr_index:,}/{index:,} graphs...', end='\r')
+            print(f'Skipped {curr_index + 1:,}/{index + 1:,} graphs...', end='\r')
         if curr_index == index:
             print()
             break
@@ -94,25 +94,25 @@ def skip_graphs_until(graphs, index):
 
 def skip_processed_graphs(graphs):
     last_processed = get_last_processed_index()
-    if last_processed > 0:
-        print(f'Skipping first {last_processed:,} graphs...')
+    if last_processed > -1:
+        print(f'Skipping first {last_processed + 1:,} graphs...')
         skip_graphs_until(graphs, last_processed)
     return last_processed
 
 
 def fill_csvs_paralelly(n=10):
-    graphs_dict = graphs_by_thinness_precomputed(n=n-1)
+    graphs_dict = graphs_by_thinness_precomputed(n-1)
     graphs_dict.setdefault(int(n/2), [])
     graphs = connected_graphs_upto(n, start=n)
-    graphs_skipped = skip_processed_graphs(graphs)
+    last_skipped_graph = skip_processed_graphs(graphs)
     
     start_time = datetime.today()
     with mp.Pool() as pool:
         params = ((G, graphs_dict) for G in graphs)
         process_map = pool.imap(process_graph, params, chunksize=CHUNK_SIZE)
-        for index, (graph6, thinness, is_minimal) in enumerate(process_map, start=graphs_skipped + 1):
+        for index, (graph6, thinness, is_minimal) in enumerate(process_map, start=last_skipped_graph + 1):
             if is_minimal:
                 save_graph_with_thinness(graph6, thinness)
                 print_found_graph(graph6, thinness)
             save_last_processed_index(index)
-            print_updated_progress(index, graphs_skipped, start_time)
+            print_updated_progress(index, last_skipped_graph, start_time)
