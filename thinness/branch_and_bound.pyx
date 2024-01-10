19dea97d16727b80b57a80995807bc3f797ad8d2
Eric Brandwein <brandweineric@gmail.com>
2024-01-10 19:19:48 -0300

    Normalize frozenset in branch and bound

--- thinness/branch_and_bound.pyx
+++ thinness/branch_and_bound.pyx
@@ -21,6 +21,8 @@ def calculate_thinness_of_connected_graph(graph: Graph, lower_bound: int = 1, up
     if upper_bound == 1:
         return 1
 
+    cdef max_branch_and_bound_thinness = upper_bound - 1
+
     cdef binary_matrix_t adjacency_matrix
     dense_graph_init(adjacency_matrix, graph)
     
@@ -35,15 +37,17 @@ def calculate_thinness_of_connected_graph(graph: Graph, lower_bound: int = 1, up
     bitset_complement(suffix_vertices, suffix_vertices)
 
     cdef int* part_of = <int*>sig_malloc(sizeof(int) * adjacency_matrix.n_cols)
+    cdef int* parts_rename = <int*>sig_malloc(sizeof(int) * max_branch_and_bound_thinness)
 
     cdef binary_matrix_t parts_for_vertices
-    binary_matrix_init(parts_for_vertices, adjacency_matrix.n_cols, upper_bound - 1)
+    binary_matrix_init(parts_for_vertices, adjacency_matrix.n_cols, max_branch_and_bound_thinness)
     
     cdef set prefixes_seen = set() 
 
     branch_and_bound_thinness = _branch_and_bound(
         graph=adjacency_matrix,
         part_of=part_of,
+        parts_rename=parts_rename,
         parts_used=0,
         prefix_vertices=prefix_vertices,
         suffix_vertices=suffix_vertices,
@@ -51,7 +55,7 @@ def calculate_thinness_of_connected_graph(graph: Graph, lower_bound: int = 1, up
         parts_for_vertices=parts_for_vertices,
         prefixes_seen=prefixes_seen,
         lower_bound=lower_bound,
-        upper_bound=upper_bound - 1
+        upper_bound=max_branch_and_bound_thinness
     )
 
     binary_matrix_free(adjacency_matrix)
@@ -59,6 +63,7 @@ def calculate_thinness_of_connected_graph(graph: Graph, lower_bound: int = 1, up
     bitset_free(suffix_vertices)
     bitset_free(suffix_non_neighbors_of_vertex)
     sig_free(part_of)
+    sig_free(parts_rename)
     binary_matrix_free(parts_for_vertices)
 
     return branch_and_bound_thinness if branch_and_bound_thinness != -1 else upper_bound
@@ -67,6 +72,7 @@ def calculate_thinness_of_connected_graph(graph: Graph, lower_bound: int = 1, up
 cdef int _branch_and_bound(
     binary_matrix_t graph,
     int* part_of,
+    int* parts_rename,
     int parts_used,
     bitset_t prefix_vertices,
     bitset_t suffix_vertices,
@@ -79,7 +85,7 @@ cdef int _branch_and_bound(
     if bitset_isempty(suffix_vertices) and parts_used <= upper_bound:
         return parts_used
 
-    cdef frozenset prefix_with_partition = _build_prefix_with_partition(prefix_vertices, part_of)
+    cdef frozenset prefix_with_partition = _build_prefix_with_partition(prefix_vertices, part_of, parts_rename, parts_used)
     if prefix_with_partition in prefixes_seen:
         return -1
     else:
@@ -101,7 +107,8 @@ cdef int _branch_and_bound(
                 part_of[vertex] = part
                 current_solution = _branch_and_bound(
                     graph, 
-                    part_of, 
+                    part_of,
+                    parts_rename,
                     current_parts_used,
                     prefix_vertices,
                     suffix_vertices, 
@@ -125,11 +132,23 @@ cdef int _branch_and_bound(
     return best_solution_found
 
 
-cdef inline object _build_prefix_with_partition(bitset_t prefix_vertices, int* part_of):
-    cdef int prefix_vertex = bitset_next(prefix_vertices, 0)
+cdef inline object _build_prefix_with_partition(bitset_t prefix_vertices, int* part_of, int* parts_rename, int parts_used):
+    for part in range(parts_used):
+        parts_rename[part] = -1
+    
+    cdef int current_part = 0
     cdef list prefix_with_partition = []
+    cdef int prefix_vertex = bitset_next(prefix_vertices, 0)
+    cdef int part_of_vertex
     while prefix_vertex != -1:
-        prefix_with_partition.append((prefix_vertex, part_of[prefix_vertex]))
+        part_of_vertex = part_of[prefix_vertex]
+        if parts_rename[part_of_vertex] == -1:
+            parts_rename[part_of_vertex] = current_part
+            part_of_vertex = current_part
+            current_part += 1
+        else:
+            part_of_vertex = parts_rename[part_of_vertex]
+        prefix_with_partition.append((prefix_vertex, part_of_vertex))
         prefix_vertex = bitset_next(prefix_vertices, prefix_vertex + 1)
     return frozenset(prefix_with_partition) 
 
