3b3c22b7d19651c573e2b1d8b2c92a1402404aaf
Eric Brandwein <brandweineric@gmail.com>
2024-01-15 19:45:41 -0300

    Timing on random graphs

--- thinness/time_branch_and_bound.py
+++ thinness/time_branch_and_bound.py
@@ -1,17 +1,17 @@
 import timeit
 import itertools
+import textwrap
 
 from sage.graphs.graph_generators import graphs
-from sage.graphs.graph import Graph 
+from sage.graphs.graph import Graph
+from sage.misc.randstate import set_random_seed
 
 from thinness.branch_and_bound import calculate_thinness_with_branch_and_bound
 
 
-def profile():
-    n = 15
-    for _ in range(100):
-        graph = graphs.RandomGNP(n, 0.5)
-        calculate_thinness_with_branch_and_bound(graph)
+def profile(n):
+    graph = graphs.RandomGNP(n, 0.8)
+    calculate_thinness_with_branch_and_bound(graph, max_seen_entries=1_600_000)
 
 
 def crown_graph(vertices_per_side: int) -> Graph:
@@ -23,11 +23,33 @@ def crown_graph(vertices_per_side: int) -> Graph:
 
 def thinness_of_chordal_graphs(n):
     for graph in graphs.nauty_geng(f'-c -T {n}'):
-        calculate_thinness_with_branch_and_bound(graph)
+        calculate_thinness_with_branch_and_bound(graph, max_seen_entries=1_600_000)
 
 
 if __name__ == '__main__':
-    for n in range(1, 10):
+    set_random_seed(0)
+    print("Crown graphs:")
+    for n in range(2, 12):
         graph = crown_graph(n)
-        print(n, timeit.timeit("calculate_thinness_with_branch_and_bound(graph, max_prefix_length=100, max_seen_entries=1_600_000)", globals=globals(), number=1))
+        print(textwrap.indent(
+            text=f'{n}:\t{timeit.timeit("calculate_thinness_with_branch_and_bound(graph, max_seen_entries=1_600_000)", globals=globals(), number=1)}',
+            prefix='  '
+        ))
+    set_random_seed(0)
+    print("Random GNM graphs:")
+    for n in range(10, 20):
+        print(textwrap.indent(text=f'{n}:', prefix='  '))
+        for density in [0.2, 0.5, 0.8]:
+            m = n*(n-1)//2 * density
+            times = []
+            for _ in range(10):
+                graph = graphs.RandomGNM(n, m)
+                times.append(
+                    timeit.timeit("calculate_thinness_with_branch_and_bound(graph, max_seen_entries=1_600_000)", globals=globals(), number=1)
+                )
+            print(textwrap.indent(
+                text=f'density {density}: {sum(times)/len(times)}',
+                prefix='    '
+            ))
+
     
