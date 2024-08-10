1f4ef571d9e0892b44d1b410c8cdcc0fbbf3b3c2
Eric Brandwein <brandweineric@gmail.com>
2024-08-10 11:49:30 -0300

    Add batched

--- thinness/itertools_utils.py
+++ thinness/itertools_utils.py
@@ -3,3 +3,12 @@ import itertools
 def skip_first(iterator, elements):
     next(itertools.islice(iterator, elements, elements), None)
 
+
+def batched(iterable, n):
+    "Batch data into tuples of length n. The last batch may be shorter."
+    # batched('ABCDEFG', 3) --> ABC DEF G
+    if n < 1:
+        raise ValueError('n must be at least one')
+    it = iter(iterable)
+    while batch := tuple(itertools.islice(it, n)):
+        yield batch
